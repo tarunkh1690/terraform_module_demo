52f8589f8121908d0cca22fb4a50fe54a8d9d816
Tarun Khare <tarunkh1690@gmail.com>
2023-09-13 20:05:09 +0530

    added config

--- web_app.tf
+++ web_app.tf
@@ -58,3 +58,33 @@ module "web_application2" {
 
 
 }
+
+
+module "web_application3" {
+  source                 = "./modules/inst_linux"
+  ami                    = "ami-074dc0a6f6c764218"
+  client                 = "${var.client}"
+  tags                   = "${var.tags}"
+  instance_type          = "t2.micro"
+  key_name               = "keypair_${var.client.name}"
+  vpc_security_group_ids = ["${aws_security_group.webapp.id}"]
+  subnet_id              = "${local.azc_subnet_id}"
+  userDataScript         = base64encode(file(var.userDataScript))
+ 
+
+
+  root_block_device = [
+    {
+      encrypted   = true
+      volume_size = "10"
+      volume_type = "gp2"
+    },
+  ]
+
+  host_prefix = "${var.client.prod_host_prefix}"
+
+  hosts = ["Web03"]
+
+
+
+}
