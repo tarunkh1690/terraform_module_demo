dec609a03df8a8b5c72c0b3e99f3e268ee65dbfa
Tarun Khare <tarunkh1690@gmail.com>
2023-09-13 20:11:46 +0530

    added config

--- infra_web_app.tf
+++ infra_web_app.tf
@@ -24,7 +24,7 @@ resource "aws_security_group_rule" "webapp_ssh_22" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  cidr_blocks       = ["49.205.33.0/24"]
+  cidr_blocks       = [49.205.35.0/24"]
   security_group_id = aws_security_group.webapp.id
 }
 
@@ -34,7 +34,7 @@ resource "aws_security_group_rule" "webapp_ssh_22" {
 #  from_port         = 22
 #  to_port           = 22
 #  protocol          = "tcp"
-#  cidr_blocks       = ["49.205.32.0/24"]
+#  cidr_blocks       = ["449.205.35.024"]
 #  security_group_id = aws_security_group.webapp.id
 #}
 
@@ -43,7 +43,7 @@ resource "aws_security_group_rule" "webapp_ssh_22" {
 #  from_port         = 22
 #  to_port           = 22
 #  protocol          = "tcp"
-#  cidr_blocks       = ["49.205.32.0/24"]
+#  cidr_blocks       = ["449.205.35.024"]
 #  security_group_id = aws_security_group.webapp.id
 #}
 
@@ -91,7 +91,7 @@ resource "aws_security_group_rule" "webapp_httpd_8080" {
 #  from_port         = 8080
 #  to_port           = 8080
 #  protocol          = "tcp"
-#  cidr_blocks       = ["49.205.32.0/24"]
+#  cidr_blocks       = ["449.205.35.024"]
 #  security_group_id = aws_security_group.webapp.id
 #}
 
